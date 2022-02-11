93bdfb49776be7aa662e29cedf17bccb9960272b
Rael Calitro <rael06@hotmail.fr>
2022-02-11 10:49:14 +0100

    add authenticated trials controller unit test

--- tests/YLunchApi.UnitTests/Controllers/TrialsControllerTest.cs
+++ tests/YLunchApi.UnitTests/Controllers/TrialsControllerTest.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
+using YLunchApi.Authentication.Models;
+using YLunchApi.Domain.UserAggregate;
 using YLunchApi.Main.Controllers;
+using YLunchApi.UnitTests.Application.Authentication;
 
 namespace YLunchApi.UnitTests.Controllers;
 
@@ -27,5 +30,28 @@ public class TrialsControllerTest
         var responseBody = Assert.IsType<string>(responseResult.Value);
 
         responseBody.Should().Be("YLunchApi is running, you are anonymous");
+    }
+
+    [Fact]
+    public void GetAuthenticatedTry_Should_Return_A_200Ok()
+    {
+        // Assert
+        var authenticatedUserInfo = new ApplicationSecurityToken(TokenMocks.ValidAccessToken);
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers.Authorization = $"Bearer {TokenMocks.ValidAccessToken}";
+        var httpContextAccessor = new Mock<IHttpContextAccessor>();
+        httpContextAccessor.SetupProperty(x => x.HttpContext, httpContext);
+
+        var controller = new TrialsController(httpContextAccessor.Object);
+
+        // Act
+        var response = controller.GetAuthenticatedTry();
+
+        // Assert
+        var responseResult = Assert.IsType<OkObjectResult>(response.Result);
+        var responseBody = Assert.IsType<string>(responseResult.Value);
+
+        responseBody.Should().BeEquivalentTo(
+            $"YLunchApi is running, you are authenticated as {authenticatedUserInfo.UserEmail} with Id: {authenticatedUserInfo.UserId} and Roles: {Roles.ListToString(authenticatedUserInfo.UserRoles)}");
     }
 }
